08bdaa57bbd9320faaddda5bbc82afdbd73f89d1
kieu2812 <kieu2812@gmail.com>
2021-07-07 18:59:05 -0400

    Create a bone project in git hub

---
+++ src/test/java/com/galvanize/invoicify/InvoicifyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.galvanize.invoicify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Context loaded  successfully");
+	}
+
+}
