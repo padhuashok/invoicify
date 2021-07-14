2f7935623d8f76a9ac25933b35e925bd004a3bbe
Padmashree Ashokkumar <padhu2192@gmail.com>
2021-07-14 19:24:36 -0400

    Add RestDocs and documentation

--- src/test/java/com/galvanize/invoicify/integration/MockedRepoTests.java
+++ src/test/java/com/galvanize/invoicify/integration/MockedRepoTests.java
@@ -5,32 +5,67 @@ import com.galvanize.invoicify.domain.InvoiceItem;
 import com.galvanize.invoicify.domain.Item;
 import com.galvanize.invoicify.dto.ItemDto;
 import com.galvanize.invoicify.repository.InvoiceItemRepository;
+import com.galvanize.invoicify.repository.InvoiceRepository;
+import com.galvanize.invoicify.repository.ItemRepository;
+import com.galvanize.invoicify.utils.InvoicifyStringUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class MockedRepoTests{
-    @MockBean
+    @Autowired
     private MockMvc mvc;
     @MockBean
     InvoiceItemRepository invoiceItemRepository;
+    @MockBean
+    ItemRepository itemRepository;
+    @MockBean
+    InvoiceRepository invoiceRepository;
     @Test
     public void addItemToInvoice() throws Exception{
-//        Invoice invoice=new Invoice();
-//        Item item1 = new Item("DevItems",5,25);
-//        Item item2 = new Item("Dev Items More", 2, 10);
-//        InvoiceItem invoiceItem1=new InvoiceItem(item1,invoice);
-//        InvoiceItem invoiceItem2=new InvoiceItem(item2,invoice);
-//        List<InvoiceItem> items=new ArrayList<>();
-//        when(invoiceItemRepository.saveAll(items)).thenReturn(items);
-
+        Invoice invoice=new Invoice();
+        invoice.setInvoiceTotal(250);
+        Item item1 = new Item("DevItems",5,25);
+        Item item2 = new Item("Dev Items More", 2, 10);
+        InvoiceItem invoiceItem1=new InvoiceItem(item1,invoice);
+        InvoiceItem invoiceItem2=new InvoiceItem(item2,invoice);
+        List<Item> itemList =  Arrays.asList(item1,item2);
+        List<InvoiceItem> invoiceItems=new ArrayList<>();
+        when(invoiceRepository.save(invoice)).thenReturn(invoice);
+        when(itemRepository.saveAll(itemList)).thenReturn(itemList);
+        when(invoiceItemRepository.saveAll(invoiceItems)).thenReturn(invoiceItems);
+        mvc.perform(post("/invoice")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(InvoicifyStringUtils.asJsonString(invoiceItems)))
+                .andExpect(status().isCreated()).andDo((document("Add Items to Invoice POST", responseFields(
+                fieldWithPath("[]").description("One or more line invoiceItems of invoice"),
+                fieldWithPath("[].id").description("Internal ID of Items"),
+                fieldWithPath("[].description").description("Description of line item for invoice"),
+                fieldWithPath("[].quantity").description("Number of persons involved for the work"),
+                fieldWithPath("[].isFlatFee").description("Identifies the type of cost ( flat/rate based)"),
+                fieldWithPath("[].amountFlatFee").description("Flat fee Amount charged for an item "),
+                fieldWithPath("[].rateFee").description("Rate per person involved in the work "),
+                fieldWithPath("[].quantityFee").description("Amount for each person involved")))));
     }
 
 }
--- src/test/java/com/galvanize/invoicify/service/ApiTests.java
+++ src/test/java/com/galvanize/invoicify/service/ApiTests.java
@@ -8,6 +8,7 @@ import com.galvanize.invoicify.domain.*;
 import com.galvanize.invoicify.dto.ItemDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -16,10 +17,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
+@AutoConfigureRestDocs
 public class ApiTests {
     @Autowired
     private MockMvc mvc;
@@ -48,6 +53,17 @@ public class ApiTests {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(asJsonString(dtoitems)))
                 .andExpect(status().isCreated());
+                /*.andDo((document("Add Items to Invoice POST", responseFields(
+                fieldWithPath("[]").description("One or more line items of invoice"),
+                fieldWithPath("[].id").description("Invoice Item ID"),
+                fieldWithPath("[].item.id").description("Internal ID of Items"),
+                fieldWithPath("[].item.description").description("Description of line item for invoice"),
+                fieldWithPath("[].item.quantity").description("Number of persons involved for the work"),
+                fieldWithPath("[].item.totalFee").description("Identifies the type of cost ( flat/rate based)"),
+                fieldWithPath("[].item.invoiceItems").description("Flat fee Amount charged for an item "),
+                fieldWithPath("[].invoice.id").description("Rate per person involved in the work "),
+                fieldWithPath("[].invoice.invoiceItems").description("Amount for each person involved"),
+                fieldWithPath("[].invoice.invoiceTotal").description("Rate per person involved in the work ")))));*/
     }
     static String asJsonString(final Object obj) {
         try {
